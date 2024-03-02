cfdf21973979379e78ef4274e8678ce5aab12e7e
Omar Faruqe <faruqe.cse@gmail.com>
2024-03-02 10:13:43 +0600

    Calling sum and division methods from main using objects

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,13 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World");
         System.out.println("স্বাগতম");
+
+        HelloWorld helloWorld = new HelloWorld();
+        int resultOfSum = helloWorld.sumTwoNumbers(4, 5);
+        int resultOfDivision = helloWorld.division(6, 7);
+
+        System.out.println("Sum Result: " + resultOfSum + 
+            "Div Result: " + resultOfDivision);
     }
     public int sumTwoNumbers(int firstNumber, int secondNumber){
         int result;
