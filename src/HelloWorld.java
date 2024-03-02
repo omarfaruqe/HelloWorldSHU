c8156a2209ca3fbe2c45b6cabf9b926e098ba503
Omar Faruqe <faruqe.cse@gmail.com>
2024-03-02 09:48:38 +0600

    Summation method added which can take two integers and return its sum value.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,10 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("স্বাগতম");
     }
+
+    public int sumTwoNumbers(int firstNumber, int secondNumber){
+        int result;
+        result = firstNumber + secondNumber;
+        retun result;
+    }
 }
